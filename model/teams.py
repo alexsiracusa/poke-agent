a607e9dea1a5d65954dce91662f78345345c5d3d
alexsiracusa <alexander.siracusa@gmail.com>
2025-08-22 22:48:32 -0400

    trying out poke_env

---
+++ model/teams.py
@@ -0,0 +1,110 @@
+team_1 = """
+Goodra (M) @ Assault Vest
+Ability: Sap Sipper
+EVs: 248 HP / 252 SpA / 8 Spe
+Modest Nature
+IVs: 0 Atk
+- Dragon Pulse
+- Flamethrower
+- Sludge Wave
+- Thunderbolt
+
+Dragonite @ Focus Sash
+Ability: Inner Focus
+EVs: 252 Atk / 4 SpD / 252 Spe
+Jolly Nature
+- Roar
+
+Toxtricity (M) @ Throat Spray
+Ability: Punk Rock
+EVs: 4 Atk / 252 SpA / 252 Spe
+Rash Nature
+- Overdrive
+- Boomburst
+- Shift Gear
+- Fire Punch
+
+Cloyster @ Leftovers
+Ability: Skill Link
+EVs: 252 Atk / 4 SpD / 252 Spe
+Adamant Nature
+- Icicle Spear
+- Rock Blast
+- Ice Shard
+- Shell Smash
+
+Corviknight (M) @ Leftovers
+Ability: Pressure
+EVs: 248 HP / 80 SpD / 180 Spe
+Impish Nature
+- Defog
+- Brave Bird
+- Roost
+- U-turn
+
+Galvantula @ Focus Sash
+Ability: Compound Eyes
+EVs: 252 SpA / 4 SpD / 252 Spe
+Timid Nature
+IVs: 0 Atk
+- Sticky Web
+- Thunder Wave
+- Thunder
+- Energy Ball
+"""
+
+
+
+team_2 = """
+Goodra (M) @ Assault Vest
+Ability: Sap Sipper
+EVs: 248 HP / 252 SpA / 8 Spe
+Modest Nature
+IVs: 0 Atk
+- Dragon Pulse
+- Flamethrower
+- Sludge Wave
+- Thunderbolt
+
+Dragonite @ Focus Sash
+Ability: Inner Focus
+EVs: 252 Atk / 4 SpD / 252 Spe
+Jolly Nature
+- Roar
+
+Cloyster @ Leftovers
+Ability: Skill Link
+EVs: 252 Atk / 4 SpD / 252 Spe
+Adamant Nature
+- Icicle Spear
+- Rock Blast
+- Ice Shard
+- Shell Smash
+
+Sandaconda @ Focus Sash
+Ability: Sand Spit
+EVs: 252 Atk / 4 SpD / 252 Spe
+Jolly Nature
+- Stealth Rock
+- Glare
+- Earthquake
+- Rock Tomb
+
+Excadrill @ Focus Sash
+Ability: Sand Rush
+EVs: 252 Atk / 4 SpD / 252 Spe
+Adamant Nature
+- Iron Head
+- Rock Slide
+- Earthquake
+- Rapid Spin
+
+Cinccino @ Leftovers
+Ability: Skill Link
+EVs: 252 Atk / 4 Def / 252 Spe
+Jolly Nature
+- Bullet Seed
+- Knock Off
+- Rock Blast
+- Tail Slap
+"""
---
+++ model/test_poke_env.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from poke_env import RandomPlayer
+from poke_env.player import Player
+from poke_env.data import GenData
+from poke_env.battle import Battle
+
+from teams import team_1, team_2
+
+
+class MaxDamagePlayer(Player):
+    def choose_move(self, battle: Battle):
+        for name, pokemon in battle.opponent_team.items():
+            print(pokemon.moves)
+        print("")
+
+        print(battle.available_moves)
+
+        if battle.available_moves:
+            best_move = max(battle.available_moves, key=lambda move: move.base_power)
+
+            if battle.can_tera:
+                return self.create_order(best_move, terastallize=True)
+
+            return self.create_order(best_move)
+        else:
+            return self.choose_random_move(battle)
+
+
+player = MaxDamagePlayer(battle_format="gen9ou", team=team_1)
+random_player = RandomPlayer(battle_format="gen9ou", team=team_2)
+
+asyncio.run(player.battle_against(random_player, n_battles=10))
+
+print(f"Max damage player won {player.n_won_battles} / 10 battles")
+
